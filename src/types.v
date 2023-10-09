1721eed4b91bf6827e095564bcd60e3759c649f9
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-10-09 06:43:51 +0100

    refactor: add documentation

--- src/types.v
+++ src/types.v
@@ -1,18 +1,24 @@
 module main
 
+// Default epsilon value for floating point comparisons
 pub const eps = 1e-7
 
+// DataRow represents a single row of data in a TabularDataSet
 pub struct DataRow {
 pub:
 	data []string
 }
 
+// TabularDataSet represents a set of data in a tabular format.
+// The dataset has single list of headers and rows of data.
 pub struct TabularDataSet {
 pub:
 	headers []string
 	rows    []DataRow
 }
 
+// Matrix represents a 2D matrix of floating point numbers.
+// Internally, the data represented as a single array of f64 values.
 pub struct Matrix {
 pub:
 	rows int
@@ -21,6 +27,8 @@ mut:
 	data []f64
 }
 
+// LinearRegression encapsulates the coefficients and attributes of a linear
+// regression model after training on a dataset.
 pub struct LinearRegression {
 	attributes []string
 	theta      Matrix
